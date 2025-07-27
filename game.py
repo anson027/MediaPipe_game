e48b0b2d2e441b7e60f313e5483136a52c1f00b4
anson <ansonathomas777@gmail.com>
2025-07-27 12:16:44 +0530

    edited

--- game.py
+++ game.py
@@ -6,22 +6,27 @@ import pyautogui as pg
 mpfaces=mp.solutions.face_detection
 mpdrawings=mp.solutions.drawing_utils
 
-faces=mpfaces.FaceDetection(min_detection_confidence=0.6)
-threshold=30 # in pixels 
-l=None
+mphands=mp.solutions.hands
 
+faces=mpfaces.FaceDetection(min_detection_confidence=0.6)
+hands=mphands.Hands()
+threshold=40 # in pixels 
+l=None
+sw,sh=pg.size()
 video=cv2.VideoCapture(0)
 while True:
   suc,img=video.read()
   img=cv2.flip(img,1)
   img1=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
-  result=faces.process(img1)
+
+  # Face detection - Nose tips
+  result_face=faces.process(img1)
   h,w,_=img.shape
   cx=(w//2)-50
   cy=(h//2)+50
   cv2.circle(img,(cx,cy),3,(0,0,255),10)
-  if result.detections:
-    for detection in result.detections:    
+  if result_face.detections:
+    for detection in result_face.detections:    
       keypoints=detection.location_data.relative_keypoints
       nose_tip=keypoints[2]
       nose_x=int(nose_tip.x * w)
